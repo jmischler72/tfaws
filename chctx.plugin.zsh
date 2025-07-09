7e921754a48f2074310593bbc82f13477265e1e3
Joffrey <joffrey.mischlerps@gmail.com>
2025-07-09 11:46:40 +0200

    add txt in tf echo

--- chctx.plugin.zsh
+++ chctx.plugin.zsh
@@ -1,12 +1,12 @@
 source $HOME/.zshrc_priv
 
 # - Rebind terraform aliases to use TFPATH
-alias terraform='echo $TFPATH && terraform -chdir=$TFPATH '
-alias tfi='echo $TFPATH && terraform -chdir=$TFPATH init '
-alias tfp='echo $TFPATH && terraform -chdir=$TFPATH plan '
-alias tfa='echo $TFPATH && terraform -chdir=$TFPATH apply '
-alias tfd='echo $TFPATH && terraform -chdir=$TFPATH destroy '
-alias tfs='echo $TFPATH && terraform -chdir=$TFPATH show '
+alias terraform='echo tf_path: $TFPATH && terraform -chdir=$TFPATH $@'
+alias tfi='echo tf_path: $TFPATH && terraform -chdir=$TFPATH init '
+alias tfp='echo tf_path: $TFPATH && terraform -chdir=$TFPATH plan '
+alias tfa='echo tf_path: $TFPATH && terraform -chdir=$TFPATH apply '
+alias tfd='echo tf_path: $TFPATH && terraform -chdir=$TFPATH destroy '
+alias tfs='echo tf_path: $TFPATH && terraform -chdir=$TFPATH show '
 
 change_context() {
   local profile="$1"
